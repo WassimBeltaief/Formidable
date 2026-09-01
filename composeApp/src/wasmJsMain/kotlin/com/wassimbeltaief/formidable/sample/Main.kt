a851e5fa72696fcf61bc292cc70da26f515ab491
Wassim Beltaief <beltaief.wassim@gmail.com>
2026-09-01 20:07:27 +0200

    fix: migrate CanvasBasedWindow → ComposeViewport for CMP 1.9.3 compatibility

--- composeApp/src/wasmJsMain/kotlin/com/wassimbeltaief/formidable/sample/Main.kt
+++ composeApp/src/wasmJsMain/kotlin/com/wassimbeltaief/formidable/sample/Main.kt
@@ -2,17 +2,18 @@ package com.wassimbeltaief.formidable.sample
 
 import androidx.compose.runtime.remember
 import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.window.CanvasBasedWindow
+import androidx.compose.ui.window.ComposeViewport
 import com.wassimbeltaief.formidable.sample.data.SignUpRepositoryImpl
 import com.wassimbeltaief.formidable.sample.domain.usecase.GetSignUpFormUseCase
 import com.wassimbeltaief.formidable.sample.domain.usecase.SaveSignUpFormUseCase
 import com.wassimbeltaief.formidable.sample.ui.signup.SignUpScreen
 import com.wassimbeltaief.formidable.sample.ui.signup.SignUpViewModel
 import com.wassimbeltaief.formidable.sample.ui.theme.FormidableTheme
+import kotlinx.browser.document
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
-    CanvasBasedWindow(canvasElementId = "ComposeTarget") {
+    ComposeViewport(document.body!!) {
         val repository = remember { SignUpRepositoryImpl() }
         val viewModel =
             remember {
